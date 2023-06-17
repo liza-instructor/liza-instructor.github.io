840dca6454e8c2939263ea661bd7f618d4e9361a
liza-wood <belwood@ucdavis.edu>
2023-06-17 10:37:49 -0700

    first commit

---
+++ workshop_notes.R
@@ -0,0 +1,22 @@
+# Where am I in my computer? Get working directory
+getwd()
+
+# file paths can be searched in RStudio in quotes
+data <- read.csv("citation_classifier/data/ltwa_abbr.csv")
+
+# navigate backwards
+"../../../"
+
+# write absolute file paths
+"~/Documents/Davis/R-Projects/"
+
+# access non-local files -- need to set this up
+# Box Drive
+"~/Box/d-rug/"
+# Drop box
+"~/Dropbox/"
+# Google Sheets
+
+# R Proj changes wd to the directory
+getwd()
+
